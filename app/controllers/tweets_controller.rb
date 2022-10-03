de4e74476bdf94fd907ea9e83fc4a10ebc5e5d0c
Nyoho~ <luciocanessa@gmail.com>
2022-10-03 21:04:36 +0000

    Arreglado el problema de crear tweets (Solo falta el problema del 'require')

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,13 +1,13 @@
 class TweetsController < ApplicationController
     def index
-        @tweets = Tweet.all.order(:created_at)
+        @tweets = Tweet.all.order(created_at: :desc)
     end
 
     def show
         @tweet = Tweet.find(params[:id])
     end
 
-    def new
+    def create
         @tweet = Tweet.new(tweet_params)
         if @tweet.save
             redirect_to @tweet, notice: "Se ha creado el tweet correctamente"
@@ -16,7 +16,8 @@ class TweetsController < ApplicationController
         end
     end    
     private
+
     def tweet_params
-        params.require(:tweet).permit(:monster_id, :content) //PROBLEMA ACÁ!!
+        params.permit(:monster_id, :content) #Preguntar el por que funciona con require
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :monsters, only: [:index, :show]
   resources :tweets
+  post '/tweets/new', to: 'tweets#create'
 
   root "tweets#index"
 end
